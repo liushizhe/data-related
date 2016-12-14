e9a620d6ea952ec401cadeb490a8a554c6a699c8
Jason <ljs19870826@163.com>
2016-12-14 22:27:54 +0800

    create files

---
+++ data-related.js
@@ -0,0 +1,16 @@
+function load () {
+	alert("1");
+	var finput = document.getElementById("data");
+	var editor = document.getElementById("editor");
+
+	var f = finput.files[0];
+	if(f){
+		var r = new FileReader();
+		r.onload = function(e){
+			editor.innerHTML = e.target.result;
+		}
+		r.readAsText(f);
+	} else {
+		editor.innerHTML = "Failed to load file"
+	}
+}
